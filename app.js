3a68d9bf278f763d43d9913da4a158b0a8b7c299
daiiz <da.iz.mg@gmail.com>
2017-08-01 03:29:31 +0900

    いらない

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/static', express.static(__dirname + '/static'));
 app.use('/dawiki-wiki', express.static(__dirname + '/dawiki-wiki'));
-// app.use('/dawiki-wiki/dist', express.static('dawiki-wiki/dist'));
-// app.use('/dawiki-wiki/src', express.static('dawiki-wiki/src'));
-// app.use('/dawiki-wiki/dawiki-head', express.static('dawiki-wiki/dawiki-head'));
-// app.use('/dawiki-wiki/dawiki-line', express.static('dawiki-wiki/dawiki-line'));
-// app.use('/dawiki-wiki/dawiki-paper', express.static('dawiki-wiki/dawiki-paper'));
-// app.use('/dawiki-wiki/dawiki-wiki', express.static('dawiki-wiki/dawiki-wiki'));
 
 // Settings for ejs
 app.set('views', __dirname + '/views');
