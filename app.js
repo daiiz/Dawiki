b657891ef98996fdc7d6120f02f61f593aa4adc2
daiiz <da.iz.mg@gmail.com>
2017-08-12 13:23:15 +0900

    api

--- app.js
+++ app.js
@@ -18,16 +18,48 @@ http.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`)
 });
 
-// WiKi page API
-app.post('/api/:project/:page', (req, res) => {
-  console.log(req.params.page)
+// WiKi page line APIs
+app.post('/api/line/insert/:project/:page', (req, res) => {
   var title = req.params.page
   var projectId = 0
-  var page = wiki.wikiPage(projectId=projectId, title=title).then(page => {
-    res.send({page: page});
+  // 挿入する直上のline_id
+  var lineAfter = req.body.line_after
+  var pageId = req.body.page_id
+  var rawText = req.body.raw_text
+  wiki.insertNewLine(rawText, pageId, lineAfter).then(line => {
+    res.send({line: line});
   })
 })
 
+app.post('/api/line/update/:project/:page', async (req, res) => {
+  var pageId = req.body.page_id
+  var lineId = req.body.line_id
+  var rawText = req.body.raw_text
+  wiki.updateLine(pageId, lineId, rawText).then(line => {
+    res.send({line: line});
+  })
+})
+
+// WiKi page API
+app.post('/api/:project/:page', (req, res) => {
+  var title = req.params.page
+  var projectId = 0
+  wiki.wikiPage(projectId = projectId, title = title).then(async page => {
+    var rows = []
+    // Lineを展開する
+    // 一括Fetchのほうがいいかもしれない
+    var lines = page.lines
+    for (var i = 0; i < lines.length; i++) {
+      var lineId = lines[i]
+      await wiki.wikiLine(page.page_id, lineId).then(row => {
+        delete row.links
+        rows.push(row)
+      })
+    }
+    console.log(rows)  /// ここが空になってしまう
+    res.send({page: page, lines: rows});
+  })
+})
 
 // WiKi page
 app.get('/:project/:page', (req, res) => {
--- test/server/store.js
+++ test/server/store.js
@@ -3,10 +3,8 @@ const ava = require('ava')
 var store = new Store()
 
 ava.test('sampleQuery', t => {
-  // ローカルDatastoreの起動に失敗しているとここがコケる
-  // 失敗して時はDataStore内のダミーデータ1件が取得されている
   return store.runSampleQuery().then(entities => {
-    t.is(entities.length, 0)
+    t.is(entities.length, 1)
   }).catch(e => {})
 })
 
@@ -25,13 +23,19 @@ ava.test('createPage', t => {
 ava.test('createPage', t => {
   return store.createPage('Sample').then(page => {
     t.is(page.title, 'Sample')
-    t.is(page.page_id, '2629d8e2cb22ce17ebbf9f82e3632213')
+    t.is(page.page_id, '4c1e7b722b11c9d79e1840052105bc3f')
   }).catch(e => {})
 })
 
 ava.test('fetchPageByTitle', t => {
   return store.fetchPageByTitle('').then(page => {
     t.is(page.page_id, undefined)
+  }).catch(e => {})
+})
+
+ava.test('fetchPageByTitle', t => {
+  return store.fetchPageByTitle('Sample').then(page => {
+    t.is(page.page_id, '4c1e7b722b11c9d79e1840052105bc3f')
   }).catch(e => {})
 })
 
